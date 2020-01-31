f7233a5baada54992d3f92867c21f576e357ed5e
IlyaDzialendzik <46846335+Aquam00ster@users.noreply.github.com>
2020-01-30 20:21:45 -0600

    no yellow

--- src/main/java/frc/robot/commands/DriveManuallyCommand.java
+++ src/main/java/frc/robot/commands/DriveManuallyCommand.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.Joystick;
