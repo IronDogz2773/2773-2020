a546e4f86f60eae6e71e747535f19f48b1afdb85
IlyaDzialendzik <46846335+Aquam00ster@users.noreply.github.com>
2020-02-08 11:18:44 -0600

    Navigation Subsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,42 +1,30 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.ADXL362;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
 
-    //Motors
-    private final Spark FL = new Spark(Constants.leftFrontPort); 
+    // Motors
+    private final Spark FL = new Spark(Constants.leftFrontPort);
     private final Spark FR = new Spark(Constants.rightFrontPort);
     private final Spark BL = new Spark(Constants.leftBackPort);
     private final Spark BR = new Spark(Constants.rightBackPort);
 
-    //Speed controller groups
-    private final SpeedControllerGroup leftMotors = new SpeedControllerGroup(FL, BL); 
+    // Speed controller groups
+    private final SpeedControllerGroup leftMotors = new SpeedControllerGroup(FL, BL);
     private final SpeedControllerGroup rightMotors = new SpeedControllerGroup(FR, BR);
 
-    //Differential drive
+    // Differential drive
     private final DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
 
-    //Sensors
-    public final ADXRS450_Gyro gyroscope = new ADXRS450_Gyro();
-    public final ADXL362 accelerometer = new ADXL362(Accelerometer.Range.k2G);
+    public boolean driveState = false; // when true vision is running
 
-    public boolean driveState = false; //when true vision is running
+    public DriveSubsystem() {
 
-    public DriveSubsystem(){
-        if(gyroscope.isConnected())
-        {
-            gyroscope.reset();
-            gyroscope.calibrate();
-        }
-        accelerometer.setRange(Accelerometer.Range.k2G);
         drive.setDeadband(.1);
     }
 
@@ -44,8 +32,7 @@ public class DriveSubsystem extends SubsystemBase {
         drive.arcadeDrive(speed, rotation);
     }
 
-    public void rawDrive(final double speed, final double rotation, final boolean accel)
-    {
+    public void rawDrive(final double speed, final double rotation, final boolean accel) {
         drive.arcadeDrive(speed, rotation, accel);
     }
 }
---
+++ src/main/java/frc/robot/subsystems/NavigationSubsystem.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.ADXL362;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+
+public class NavigationSubsystem extends SubsystemBase {
+
+  // Sensors
+  public final ADXRS450_Gyro gyroscope = new ADXRS450_Gyro();
+  public final ADXL362 accelerometer = new ADXL362(Accelerometer.Range.k2G);
+
+  private NetworkTableEntry angleEntry;
+
+  /**
+   * Creates a new NavigationSubsystem.
+   */
+  public NavigationSubsystem() {
+    if (gyroscope.isConnected()) {
+      gyroscope.reset();
+      gyroscope.calibrate();
+    }
+    accelerometer.setRange(Accelerometer.Range.k2G);
+
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    angleEntry = inst.getEntry("/angle");
+  }
+
+  public double getAngle(){
+    return angleEntry.getDouble(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
