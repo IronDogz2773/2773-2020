5390c4cc562b005343f4d77dfa1637d594002aed
IronDogz <irondogzrobotics@gmail.com>
2020-02-11 20:19:44 -0600

    Added ADIS16470 support to NavigationSubsystem

--- src/main/java/frc/robot/subsystems/NavigationSubsystem.java
+++ src/main/java/frc/robot/subsystems/NavigationSubsystem.java
@@ -11,14 +11,19 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.ADXL362;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.analog.adis16470.frc.ADIS16470_IMU;
+/*import com.analog.adis16470.frc.ADIS16470_IMU.IMUAxis;
+import edu.wpi.first.wpilibj.SPI.Port;*/
 
 public class NavigationSubsystem extends SubsystemBase {
 
   // Sensors
   private final ADXRS450_Gyro gyroscope = new ADXRS450_Gyro();
+  private final ADIS16470_IMU imu = new ADIS16470_IMU(ADIS16470_IMU.IMUAxis.kY, SPI.Port.kMXP, ADIS16470_IMU.ADIS16470CalibrationTime._256ms);
   private final ADXL362 accelerometer = new ADXL362(Accelerometer.Range.k2G);
 
   private NetworkTableEntry angleEntry;
@@ -33,11 +38,15 @@ public class NavigationSubsystem extends SubsystemBase {
     }
     accelerometer.setRange(Accelerometer.Range.k2G);
 
+    imu.reset();
+    imu.calibrate();
+
     NetworkTableInstance inst = NetworkTableInstance.getDefault();
     angleEntry = inst.getEntry("/angle");
 
     SmartDashboard.putData("Gyroscope", gyroscope);
     SmartDashboard.putData("Accelerometer", accelerometer);
+    SmartDashboard.putData("NewCoolIMU", imu);
   }
 
   public double getVisionAngle() {
@@ -46,6 +55,7 @@ public class NavigationSubsystem extends SubsystemBase {
 
   public double getGyroAngle() {
     return gyroscope.getAngle();
+    //return imu.getAngle();
   }
 
   public void resetGyro() {
